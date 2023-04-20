4e7050d2193e37512c761794f147b8811b9cc9c5
Albert <wanghaotang18@gmail.com>
2023-04-20 05:22:33 -0400

    remove dist

--- assets/VSpacer.ea77bf62.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./VAvatar.470f35e7.js";const a=e("flex-grow-1","div","VSpacer");export{a as V};
--- assets/lazy.501bc841.js
+++
@@ -1,1 +0,0 @@
-import{au as r,k as n,A as u,Z as f}from"./index.8428b7f6.js";const c=r({eager:Boolean},"lazy");function m(e,o){const a=n(!1),s=u(()=>a.value||e.eager||o.value);f(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{c as m,m as u};
