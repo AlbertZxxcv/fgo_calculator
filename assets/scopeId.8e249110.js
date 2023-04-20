2c9422c1229b7d552a7520407a885de7778e5e98
Albert <wanghaotang18@gmail.com>
2023-04-20 10:12:19 -0400

    add base

---
+++ assets/scopeId.8e249110.js
@@ -0,0 +1,1 @@
+import{aP as o}from"./index.5831b7bb.js";function c(){const e=o("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{c as u};
--- assets/useAppAbility.284ae326.js
+++
@@ -1,1 +0,0 @@
-import{aN as i}from"./index.8ab7e503.js";const t=()=>i();export{t as u};
